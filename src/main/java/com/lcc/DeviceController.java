b0ca45264a29487986268453ef0bfb7c25a7e45b
Johan Janssen <johanj@infosupport.com>
2016-11-26 21:14:53 +0100

    Added support for different channels and outputs.

--- src/main/java/com/lcc/DeviceController.java
+++ src/main/java/com/lcc/DeviceController.java
@@ -3,16 +3,18 @@ package com.lcc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DeviceController {
-	
+
 	@Autowired
 	private DeviceManager deviceManager;
-	
+
 	@RequestMapping("/intensity/{value}")
-	public void test(@PathVariable("value") int intensity) {
-		deviceManager.sendIntensity(intensity);
-	}	
+	public void setIntensity(@PathVariable("value") int intensity, @RequestParam(value="channel", defaultValue="0") String channel,
+			@RequestParam(value="output", defaultValue="1") String output) {
+		deviceManager.sendIntensity(intensity, channel, output);
+	}
 }
--- src/main/java/com/lcc/DeviceManager.java
+++ src/main/java/com/lcc/DeviceManager.java
@@ -7,24 +7,30 @@ import org.springframework.stereotype.Service;
 @Service
 public class DeviceManager {
 
-	private String channel = "0";
-	private String output = "1";
+	public void sendIntensity(int intensity, String channel, String output) {
+		String command = determineCommand(intensity);
 
-	public void sendIntensity(int intensity) {
+		if ("2".equals(command)) {
+			intensity = -intensity;
+		}
+		callInfraredApplication(command, String.valueOf(intensity), channel, output);
+	}
+
+	private String determineCommand(int intensity) {
 		String command;
 		if (intensity < 0) {
 			command = "2";
-			intensity = -intensity;
 		} else if (intensity > 0) {
 			command = "1";
 		} else {
 			command = "0";
 		}
-		callCApplication(command, String.valueOf(intensity));
+		return command;
 	}
 
-	private void callCApplication(String command, String intensity) {
-		String cmd = String.format("/home/pi/LCCInstallScript/infrared %s %s %s %s", channel, output, command, intensity);
+	private void callInfraredApplication(String command, String intensity, String channel, String output) {
+		String cmd = String.format("/home/pi/LCCInstallScript/infrared %s %s %s %s", channel, output, command,
+				intensity);
 		try {
 			Runtime.getRuntime().exec(cmd);
 		} catch (IOException e) {
